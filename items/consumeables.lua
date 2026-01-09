6a7fd8ac7e0e4a3c3d60d2ca666c6428de94866c
jfalder <jonah_falder@taylor.edu>
2026-01-09 11:57:00 -0500

    added enchantress

--- items/consumeables.lua
+++ items/consumeables.lua
@@ -100,3 +100,91 @@ SMODS.Consumable{
         G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
     end,
 }
+
+--enchantress
+
+local function strip_joker_mods(j)
+    if not j then return end
+
+    if j.set_edition then 
+        j:set_edition(nil, true, true)
+    else
+        j.edition = nil
+    end
+
+
+
+    if j.ability then 
+
+        j.ability.eternal = nil
+        j.ability.perishable = nil 
+        j.ability.rental = nil
+
+
+        if j.ability.perish_tally  ~= nil then j.ability.perish_tally = 0 end
+        if j.ability.perish ~= nil then j.ability.perish = 0 end
+        j.ability.rental_rate = nil
+    end
+
+
+    if j.sticker then 
+        j.sticker = nil
+    end
+
+
+    if j.juice_up then 
+        j:juice_up(0.3,0.4) 
+        
+    end
+end
+
+SMODS.Atlas{
+    key = 'enchant',
+    path = 'enchant.png',
+    px = 170,
+    py = 250,
+}
+
+
+SMODS.Consumable{
+    key = 'enchant',
+    set = 'Spectral',
+    loc_txt = {
+        name = 'Enchantress',
+        text = {
+            "Removes {C:attention}EVERY{} modification of a Random Joker",
+            "{C:red}Positive{} or {C:purple}Negative{}"
+        }
+    },
+
+    unlocked = true,
+    cost = 4,
+    atlas = 'enchant',
+    pos = {x=0.25, y=0},
+
+
+    can_use = function(self,card)
+        return G and G.jokers and G.jokers.cards and #G.jokers.cards > 0
+    end,
+
+
+    use = function(self, card, area, copier)
+        if not (G and G.jokers and G.jokers.cards) then return end
+        
+        
+        local pool ={}
+        for i=1 , #G.jokers.cards do
+            local j = G.jokers.cards[i]
+            if j then pool[#pool+1] = j end 
+        end
+        if #pool == 0 then return end
+
+        local target = pseudorandom_element(pool, pseudoseed('enchantress'))
+        if not target then return end
+
+        strip_joker_mods(target)
+    
+    end,
+
+
+}
